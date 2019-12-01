a973b48cdc36a095360b0e80d822a9ea6d646a0f
aereal <aereal@aereal.org>
2019-12-01 23:27:39 +0900

    front: extract GraphQL queries/fragments and generate their type definitions

---
+++ front/src/organisms/__generated__/ArticleFragment.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: ArticleFragment
+// ====================================================
+
+export interface ArticleFragment {
+  readonly __typename: "Article";
+  readonly title: string | null;
+}
