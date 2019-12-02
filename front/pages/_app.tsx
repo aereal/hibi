c7106eecdc0d6df18875b7944bf69a66d048fd14
aereal <aereal@aereal.org>
2019-12-03 03:59:06 +0900

    front: use theme

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import { ApolloProvider } from "@apollo/react-common";
+import { ThemeProvider } from "styled-components";
 import { WithApolloProps } from "../src/types";
 import { withApollo } from "../src/with-apollo";
+import { defaultTheme } from "../src/styling";
 
 class ApolloApp extends App<WithApolloProps<any>> {
   render(): JSX.Element {
@@ -14,7 +16,9 @@ class ApolloApp extends App<WithApolloProps<any>> {
           <title>hibi</title>
         </Head>
         <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
+          <ThemeProvider theme={defaultTheme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
         </ApolloProvider>
       </>
     );
