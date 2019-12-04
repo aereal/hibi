ae3bbc5527238c9b95f6cdbbf057d57e714fdd99
aereal <aereal@aereal.org>
2019-12-05 00:51:55 +0900

    front: BidirectionalPager

---
+++ front/src/organisms/__generated__/BidirectionalPageInfo.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: BidirectionalPageInfo
+// ====================================================
+
+export interface BidirectionalPageInfo {
+  readonly __typename: "PageInfo";
+  readonly hasNextPage: boolean;
+  readonly hasPreviousPage: boolean;
+  readonly startCursor: string | null;
+  readonly endCursor: string | null;
+}
