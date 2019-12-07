265c462ca2cd00feee41930572d63cfbbb5fc0fb
aereal <aereal@aereal.org>
2019-12-07 18:14:13 +0900

    front: reuse initialized app to avoid `app named default already exists` error

--- front/src/firebase.ts
+++ front/src/firebase.ts
@@ -1,11 +1,25 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const app = firebase.initializeApp({
-  apiKey: "TODO",
-  authDomain: "TODO",
-  projectId: "TODO",
-  appId: "TODO",
-});
+let cachedApp: firebase.app.App | undefined;
 
-export const auth = firebase.auth(app);
+const getApp = (): firebase.app.App => {
+  if (cachedApp !== undefined) {
+    return cachedApp;
+  }
+  const [initializedApp] = firebase.apps;
+  if (initializedApp !== undefined) {
+    cachedApp = initializedApp;
+    return initializedApp;
+  }
+
+  cachedApp = firebase.initializeApp({
+    apiKey: "TODO",
+    authDomain: "TODO",
+    projectId: "TODO",
+    appId: "TODO",
+  });
+  return cachedApp;
+};
+
+export const auth = firebase.auth(getApp());
