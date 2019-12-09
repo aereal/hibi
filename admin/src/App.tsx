5fb6f4da39083428fce9ceeb44ee9ecdd4e66cb8
aereal <aereal@aereal.org>
2019-12-10 00:56:50 +0900

    admin: sign-in page

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -2,11 +2,14 @@ import React, { FC, useState, useEffect } from "react";
 import { Route } from "type-route";
 import { routes, getCurrentRoute, listen } from "./routes";
 import { RootPage } from "./pages/RootPage";
+import { SignInPage } from "./pages/SignInPage";
 
 const Routing: FC<{ route: Route<typeof routes> }> = ({ route }) => {
   switch (route.name) {
     case routes.root.name:
       return <RootPage />;
+    case routes.signIn.name:
+      return <SignInPage />;
     default:
       return <>Not Found</>;
   }
--- admin/src/routes.ts
+++ admin/src/routes.ts
@@ -2,4 +2,5 @@ import { createRouter, defineRoute } from "type-route";
 
 export const { routes, listen, getCurrentRoute } = createRouter({
   root: defineRoute("/"),
+  signIn: defineRoute("/sign-in"),
 });
