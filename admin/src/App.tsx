38e9c7381815da9e85d1377a4636bc7f2a917b62
aereal <aereal@aereal.org>
2019-12-09 13:37:24 +0900

    admin: add root page

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useState, useEffect } from "react";
 import { Route } from "type-route";
 import { routes, getCurrentRoute, listen } from "./routes";
+import { RootPage } from "./pages/RootPage";
 
 const Routing: FC<{ route: Route<typeof routes> }> = ({ route }) => {
   switch (route.name) {
     case routes.root.name:
-      return null;
+      return <RootPage />;
     default:
       return <>Not Found</>;
   }
