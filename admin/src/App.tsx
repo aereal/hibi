530c959d86fa25c141e7dac440ac7b64a566f7d5
aereal <aereal@aereal.org>
2019-12-09 13:35:47 +0900

    admin: setup routing

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { FC, useState, useEffect } from "react";
+import { Route } from "type-route";
+import { routes, getCurrentRoute, listen } from "./routes";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+const Routing: FC<{ route: Route<typeof routes> }> = ({ route }) => {
+  switch (route.name) {
+    case routes.root.name:
+      return null;
+    default:
+      return <>Not Found</>;
+  }
+};
+
+const App: FC = () => {
+  const [route, setRoute] = useState(getCurrentRoute());
+  useEffect(() => listen(setRoute), []);
+
+  return <Routing route={route} />;
 };
 
 export default App;
---
+++ admin/src/routes.ts
@@ -0,0 +1,5 @@
+import { createRouter, defineRoute } from "type-route";
+
+export const { routes, listen, getCurrentRoute } = createRouter({
+  root: defineRoute("/"),
+});
