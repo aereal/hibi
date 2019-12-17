6dd4dfaff150f2bf006abe4564ffc7c1b9da3c9b
aereal <aereal@aereal.org>
2019-12-17 19:45:21 +0900

    admin: load .gql files

--- admin/config/webpack.config.js
+++ admin/config/webpack.config.js
@@ -288,9 +288,13 @@ module.exports = function(webpackEnv) {
       // https://github.com/facebook/create-react-app/issues/290
       // `web` extension prefixes have been added for better support
       // for React Native Web.
-      extensions: paths.moduleFileExtensions
-        .map(ext => `.${ext}`)
-        .filter(ext => useTypeScript || !ext.includes("ts")),
+      extensions: [
+        ...paths.moduleFileExtensions
+          .map(ext => `.${ext}`)
+          .filter(ext => useTypeScript || !ext.includes("ts")),
+        ".graphql",
+        ".gql",
+      ],
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
@@ -486,6 +490,11 @@ module.exports = function(webpackEnv) {
                 },
                 "sass-loader"
               ),
+            },
+            {
+              test: /\.(graphql|gql)$/,
+              exclude: /node_modules/,
+              loader: "graphql-tag/loader",
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
