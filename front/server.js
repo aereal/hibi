f7084da974604b42d36a4200d8420f9c9962de85
aereal <aereal@aereal.org>
2019-12-21 17:03:50 +0900

    front: custom server

---
+++ front/server.js
@@ -0,0 +1,38 @@
+const { resolve } = require("path");
+const express = require("express");
+const next = require("next");
+const { inspect } = require("util");
+
+const start = async () => {
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  const dev = process.env.NODE_ENV !== "production";
+  const conf = require("./next.config.js");
+  const app = next({ dev, conf: typeof conf === "function" ? conf({}) : conf });
+  const handle = app.getRequestHandler();
+
+  const server = express();
+  server.get("/", (req, res) => app.render(req, res, "/", req.query));
+  server.all("*", (req, res) => handle(req, res));
+
+  await new Promise((ok, ng) => {
+    const httpServer = server.listen(port, err => {
+      if (err) ng(err);
+    });
+    httpServer.on("error", ng);
+    httpServer.on("listening", () => ok());
+  });
+  await app.prepare();
+  return app;
+};
+
+const main = () => {
+  start()
+    .then(() => {
+      console.log(`---> start next dev`);
+    })
+    .catch(err => {
+      console.error(err);
+      process.nextTick(() => process.exit(1));
+    });
+};
+main();
